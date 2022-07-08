8f43b7ce23651f94f7511a94571f6851f41e99e8
dlgurwls0529 <dlgurwls0529@gmail.com>
2022-07-08 17:55:33 +0900

    first commit

--- src/Example1/FastShip.java
+++ src/Example1/FastShip.java
@@ -4,17 +4,27 @@ public class FastShip extends Ship {
 
     private int speed;
 
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
     @Override
     void checkAttribute() {
         System.out.println("- name : " + this.getName());
         System.out.println("- color : " + this.getColor());
         System.out.println("- number : " + this.getNumber());
-        System.out.println("- speed : " + this.getNumber());
+        System.out.println("- speed : " + this.getSpeed());
     }
 
     public FastShip(String name) {
         super(name);
         this.setColor("black");
+        this.setSpeed(10);
         this.setNumber(Math.abs((int)System.currentTimeMillis()));
     }
+
 }
